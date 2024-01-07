6806f8c701db6d75ff603c2ab5eafea042fcf324
Palash Sharma <palash.sharma2002@gmail.com>
2024-01-07 17:57:52 +0800

    split parser into subparsers and added ability to add custom command modules

---
+++ common/loader.py
@@ -0,0 +1,37 @@
+"""common/loader.py: Load command modules"""
+
+from __future__ import annotations
+
+from argparse import ArgumentParser
+from collections.abc import Iterable
+
+from common.command_modules import (
+    CheckStatusModule,
+    CommandModule,
+    DumpGradesModule,
+    GradeModule,
+    InspectModule,
+)
+
+
+def load_and_run_pygrader(custom_modules: Iterable[CommandModule] | None = None):
+    """Load and run the pygrader framework with the provided custom modules."""
+    modules = [GradeModule(), DumpGradesModule(), CheckStatusModule(), InspectModule()]
+    if custom_modules:
+        modules.extend(custom_modules)
+
+    load_and_run_modules(modules, description="pygrader: Python Grading Framework")
+
+
+def load_and_run_modules(modules: Iterable[CommandModule], **kwargs):
+    parser = ArgumentParser(**kwargs)
+    subparsers = parser.add_subparsers(required=True, dest="command")
+
+    modules_map = {}
+    for module in modules:
+        modules_map[module.name] = module
+        subparser = subparsers.add_parser(module.name)
+        module.extend_parser(subparser)
+
+    args = parser.parse_args()
+    modules_map[args.command].run(args)
